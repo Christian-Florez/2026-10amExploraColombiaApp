56b87a2b53ecf37b9be31ec73bc9b8847063accd
Christian-Florez <cflorez669@unab.edu.co>
2026-04-14 18:25:03 -0500

    Maquetacion Login y register

--- app/src/main/java/com/christian/exploracolombiaapp/MainActivity.kt
+++ app/src/main/java/com/christian/exploracolombiaapp/MainActivity.kt
@@ -12,36 +12,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.christian.exploracolombiaapp.ui.theme.ExploraColombiaAppTheme
+import me.christian.exploracolombiaapp.LoginScreen
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ExploraColombiaAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            LoginScreen(onLoginSuccess = {}, onNavigateToRegister = {})
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExploraColombiaAppTheme {
-        Greeting("Android")
     }
 }
